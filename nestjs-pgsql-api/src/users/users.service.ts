94c276c6d3be6d868bd5eba6d979591a81eb505f
Eduardo Pinhata <edupinhata@gmail.com>
2022-07-12 16:55:09 -0300

    initial commit

---
+++ nestjs-pgsql-api/src/users/users.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserRepository } from './user.repository';
+
+@Injectable()
+export class UsersService {
+    constructor(
+        @InjectRepository(UserRepository)
+        private userRepository: UserRepository,
+    ){}
+
+}
